d696c0fbaae0a78dd1b7e9d2236ff3a478e1d3bd
Flavio Ippolito Vasini <64874170+Vasini25@users.noreply.github.com>
2021-08-26 00:11:15 -0300

    Add files via upload

--- Thomas.py
+++ Thomas.py
@@ -1,35 +1,65 @@
-import numpy as np  
- 
-def TDMA(a,b,c,d):  
-    #preliminares  
-    a = a.astype(’double’)  
-    b = b.astype(’double’)  
-    c = c.astype(’double’)  
-    d = d.astype(’double’)  
- 
-    #recupera ordem do sistema  
-    n=np.shape(a)[0]  
- 
-    #inicializa vetor x  
-    x=np.zeros(n)  
- 
-    #calcula cl e dl sobrescrevendo-os em c e d  
-    c[0]=c[0]/b[0]  
-    for i in np.arange(1,n-1,1):  
-       c[i]=c[i]/(b[i]-a[i]*c[i-1])  
- 
-    d[0]=d[0]/b[0]  
-    for i in np.arange(1,n,1):  
-       d[i]=(d[i]-a[i]*d[i-1])/(b[i]-a[i]*c[i-1])  
- 
-    #faz substituicao reversa para obter a solucao x  
-    x[n-1]=d[n-1]  
-    for i in np.arange(n-2,-1,-1):  
-       x[i]=d[i]-c[i]*x[i+1]  
+# " Entrada: "
+# " n - dimensao da matriz A e do sistema linear Ax=b "
+# " d0 - entradas da diagonal principal da matriz SPD tridiagonal "
+# " d1 - entradas da diagonal inferior (e superior ja que simetrica) "
+# " b - vetor lado-direito do sistema Ax=b "
 
 
->>>a=np.array([1,1,1,1,1])  
->>>b=np.array([2,2,2,2,2])  
->>>c=np.array([1,1,1,1,1])  
->>>d=np.array([4,4,0,0,2])  
->>>TDMA(a,b,c,d)
+n = int(input())
+
+# Create zero matrix
+d0 = [0] * n
+d1 = [0] * (n - 1)
+d2 = [0] * (n - 1)
+b = [0] * n
+x = [0] * n
+
+for i in range(n):
+    d0[i] = float(input());
+
+for i in range(n - 1):
+    d1[i] = float(input());
+
+for i in range(n - 1):
+    d2[i] = float(input());
+
+for i in range(n):
+    b[i] = float(input());
+
+# " Neste ponto, n, A e b sao conhecidos. Voce deve adicionar o "
+# " seu codigo que resolva o sistema Ax=b atraves do algoritmo de"
+# " Thomas."
+
+for i in range(n-1):
+    if(i==0):
+        d2[i] = d2[i]/d0[i];
+    else:
+        d2[i] = d2[i]/(d0[i] - d2[i-1]*d1[i-1]);
+
+i=0;
+
+while (i<n):
+    if(i==0):
+        b[i] = b[i]/d0[i]
+        i = i+1;
+    else:
+        b[i] = (b[i] - b[i-1]*d1[i-1])/(d0[i] - d2[i-1]*d1[i-1])
+        i = i+1;
+
+i = n-1;
+
+
+while(i>=0):
+    if(i==n-1):
+        x[i] = b[i]
+        #x[i] = x[i] * (-1);
+        i = i-1;
+    else:
+        x[i] = b[i] - (d2[i]*x[i+1])
+        #x[i] = x[i]*(-1);
+        i = i-1;
+
+
+for i in range(n):
+    #print(x[i]);
+    print("%.6f" % x[i]);
